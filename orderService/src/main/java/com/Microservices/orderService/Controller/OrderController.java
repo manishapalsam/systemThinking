175db8a11c80c1183ebf5d72f8de0fd8d235d40b
manishapalsam <manishasomayya@gmail.com>
2026-01-17 23:57:03 +0530

    Logging at each stage

--- orderService/src/main/java/com/Microservices/orderService/Controller/OrderController.java
+++ orderService/src/main/java/com/Microservices/orderService/Controller/OrderController.java
@@ -1,15 +1,19 @@
 package com.Microservices.orderService.Controller;
 
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import org.slf4j.Logger;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(OrderController.class);
 
     private final RestTemplate restTemplate;
 
@@ -19,12 +23,22 @@ public class OrderController {
 @PostMapping
     public String placeOrder(){
     // 1. Call Payment Service
+
+    log.info("STEP 1: Order request received");
+
+    log.info("STEP 2: Calling Payment Service");
     String paymentResponse =
             restTemplate.postForObject(
                     "http://localhost:8082/payments",
                     null,
                     String.class
             );
+
+
+
+    log.info("STEP 3: Payment response received");
+
+    log.info("STEP 4: Calling Inventory Service");
 
     // 2. Call Inventory Service
     String inventoryResponse =
@@ -33,6 +47,7 @@ public class OrderController {
                     null,
                     String.class
             );
+    log.info("STEP 5: Inventory response received");
 
     // 3. Return final response
     return "Order placed. " +
